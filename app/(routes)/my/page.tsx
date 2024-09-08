2b52d2133db79af8680e3d144ac3c0e4ccffead7
unknown <tagaanjustinemark3@gmail.com>
2024-09-08 10:21:18 +0800

    feat: setup user page

--- app/(routes)/my/page.tsx
+++ app/(routes)/my/page.tsx
@@ -1,12 +1,26 @@
-import { SignedIn, UserButton } from '@clerk/nextjs';
-const HomePage = () => {
-  return (
-    <div>
-      <SignedIn>
-        <UserButton showName/>
-      </SignedIn>  
-    </div>
-  )
-}
+import { redirect } from "next/navigation";
+import Sidebar from "./_components/Sidebar";
+import Header from "./_components/Header";
+import { ThemeProvider } from "next-themes";
 
-export default HomePage
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <div
+      className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]"
+      suppressHydrationWarning={true}
+    >
+      <ThemeProvider attribute="class">
+        <Sidebar/>
+        <div className="flex flex-col h-screen">
+          <Header/>
+          {children}
+        </div>
+      </ThemeProvider>
+    </div>
+  );
+}
